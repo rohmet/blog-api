7e072542079e5c45bf53c635b3f2e7558a9e280f
rohmet <harahmanabdarib@gmail.com>
2025-07-13 23:09:19 +0800

    Add models directory and define Post schema using Mongoose

---
+++ models/post.model.js
@@ -0,0 +1,30 @@
+// /models/post.model.js
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Definisikan "cetak biru" untuk postingan blog
+const postSchema = new Schema({
+  title: {
+    type: String,
+    required: true, // Judul wajib diisi
+    trim: true      // Menghapus spasi di awal dan akhir
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true,
+    trim: true
+  }
+}, {
+  timestamps: true // Otomatis menambahkan field createdAt dan updatedAt
+});
+
+// Buat Model berdasarkan Schema
+// 'Post' akan menjadi nama koleksi di MongoDB (secara otomatis menjadi jamak: 'posts')
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
