b6d8c8b55ab3e5690114fc22729cd95cd6c0569c
rohmet <harahmanabdarib@gmail.com>
2025-07-14 22:53:08 +0800

    Protect post routes: only logged-in users can create, update, delete posts

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const postController = require('../controllers/post.controller.js');
+const {
+  getAllPosts,
+  createPost,
+  getPostById,
+  updatePost,
+  deletePost,
+} = require('../controllers/post.controller.js');
+const { protect } = require('../middleware/auth.middleware.js'); // <-- Impor protect
 
-// Rute untuk mendapatkan semua post & membuat post baru
-router.route('/')
-  .get(postController.getAllPosts)
-  .post(postController.createPost);
+// Rute Publik (siapa saja bisa lihat)
+router.route('/').get(getAllPosts);
+router.route('/:id').get(getPostById);
 
-// Rute untuk mendapatkan, memperbarui, & menghapus post tunggal
-router.route('/:id')
-  .get(postController.getPostById)
-  .put(postController.updatePost)
-  .delete(postController.deletePost);
+// Rute Terproteksi (hanya pengguna yang login bisa akses)
+router.route('/').post(protect, createPost);
+router.route('/:id').put(protect, updatePost).delete(protect, deletePost);
 
 module.exports = router;
